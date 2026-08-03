ec22b811702bc5db17177eb2b0b999375cc82490
error-security-failure <attorney.ronald.hansen@gmail.com>
2026-08-03 14:16:21 -0400

    Update default title in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Microsoft DosAttack Failure"
+  const title = config.title ?? "Welcome to the Pawselite Family"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
